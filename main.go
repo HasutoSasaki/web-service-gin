4dd260f2b29ebb31cf1042e1c0bf7132037e2748
HasutoSasaki <hasuto.sa@gmail.com>
2025-07-18 23:20:43 +0900

    feat: initialize web service with album management

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "The Dark Side of the Moon", Artist: "Pink Floyd", Price: 12.99},
+	{ID: "2", Title: "Abbey Road", Artist: "The Beatles", Price: 14.99},
+	{ID: "3", Title: "Back in Black", Artist: "AC/DC", Price: 10.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+}
